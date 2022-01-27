82bcbae74a34e4b1cf5de3206516662b0cd94a01
Raúl Heredia <raul.heredia01@outlook.com>
2022-01-27 17:01:15 +0100

    Added series module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 // COMPONENTS
 import { NavbarComponent } from './navbar/navbar.component';
+import { SeriesComponent } from './series/series.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
